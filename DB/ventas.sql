14cbd761a0b959a30da1f62c8636ff7d14acd53b
denilson1817 <denilson_peco@hotmail.com>
2025-03-18 21:03:15 -0600

    [FIX] corrección BD, se reciben nulos en la tabla Productos

--- DB/ventas.sql
+++ DB/ventas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:3306
--- Tiempo de generación: 21-09-2023 a las 23:37:41
--- Versión del servidor: 5.7.33
--- Versión de PHP: 8.1.10
+-- Servidor: localhost
+-- Tiempo de generación: 19-03-2025 a las 03:55:38
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `caja` (
   `caja_id` int(5) NOT NULL,
   `caja_numero` int(5) NOT NULL,
-  `caja_nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `caja_nombre` varchar(100) NOT NULL,
   `caja_efectivo` decimal(30,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
@@ -39,7 +39,7 @@ CREATE TABLE `caja` (
 --
 
 INSERT INTO `caja` (`caja_id`, `caja_numero`, `caja_nombre`, `caja_efectivo`) VALUES
-(1, 1, 'Caja Principal', '0.00');
+(1, 1, 'Caja Principal', 2580.00);
 
 -- --------------------------------------------------------
 
@@ -49,9 +49,16 @@ INSERT INTO `caja` (`caja_id`, `caja_numero`, `caja_nombre`, `caja_efectivo`) VA
 
 CREATE TABLE `categoria` (
   `categoria_id` int(7) NOT NULL,
-  `categoria_nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `categoria_ubicacion` varchar(150) COLLATE utf8_spanish2_ci NOT NULL
+  `categoria_nombre` varchar(50) NOT NULL,
+  `categoria_ubicacion` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`categoria_id`, `categoria_nombre`, `categoria_ubicacion`) VALUES
+(1, 'Denilson', 'Veracruz');
 
 -- --------------------------------------------------------
 
@@ -61,15 +68,15 @@ CREATE TABLE `categoria` (
 
 CREATE TABLE `cliente` (
   `cliente_id` int(10) NOT NULL,
-  `cliente_tipo_documento` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_numero_documento` varchar(35) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_apellido` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_provincia` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_ciudad` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_direccion` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_telefono` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `cliente_email` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
+  `cliente_tipo_documento` varchar(20) NOT NULL,
+  `cliente_numero_documento` varchar(35) NOT NULL,
+  `cliente_nombre` varchar(50) NOT NULL,
+  `cliente_apellido` varchar(50) NOT NULL,
+  `cliente_provincia` varchar(30) NOT NULL,
+  `cliente_ciudad` varchar(30) NOT NULL,
+  `cliente_direccion` varchar(70) NOT NULL,
+  `cliente_telefono` varchar(20) NOT NULL,
+  `cliente_email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
@@ -87,11 +94,18 @@ INSERT INTO `cliente` (`cliente_id`, `cliente_tipo_documento`, `cliente_numero_d
 
 CREATE TABLE `empresa` (
   `empresa_id` int(11) NOT NULL,
-  `empresa_nombre` varchar(90) COLLATE utf8_spanish2_ci NOT NULL,
-  `empresa_telefono` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `empresa_email` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `empresa_direccion` varchar(100) COLLATE utf8_spanish2_ci NOT NULL
+  `empresa_nombre` varchar(90) NOT NULL,
+  `empresa_telefono` varchar(20) NOT NULL,
+  `empresa_email` varchar(50) NOT NULL,
+  `empresa_direccion` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`empresa_id`, `empresa_nombre`, `empresa_telefono`, `empresa_email`, `empresa_direccion`) VALUES
+(1, 'Bazar libertad', '223445569', 'bazarlibertad@gmail.com', 'av libertad');
 
 -- --------------------------------------------------------
 
@@ -101,18 +115,29 @@ CREATE TABLE `empresa` (
 
 CREATE TABLE `producto` (
   `producto_id` int(20) NOT NULL,
-  `producto_codigo` varchar(77) COLLATE utf8_spanish2_ci NOT NULL,
-  `producto_nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `producto_codigo` varchar(77) NOT NULL,
+  `producto_nombre` varchar(100) NOT NULL,
   `producto_stock_total` int(25) NOT NULL,
-  `producto_tipo_unidad` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `producto_precio_compra` decimal(30,2) NOT NULL,
+  `producto_tipo_unidad` varchar(20) DEFAULT NULL,
+  `producto_precio_compra` decimal(30,2) DEFAULT NULL,
   `producto_precio_venta` decimal(30,2) NOT NULL,
-  `producto_marca` varchar(35) COLLATE utf8_spanish2_ci NOT NULL,
-  `producto_modelo` varchar(35) COLLATE utf8_spanish2_ci NOT NULL,
-  `producto_estado` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `producto_foto` varchar(500) COLLATE utf8_spanish2_ci NOT NULL,
+  `producto_marca` varchar(35) DEFAULT NULL,
+  `producto_modelo` varchar(35) DEFAULT NULL,
+  `producto_estado` varchar(20) NOT NULL,
+  `producto_foto` varchar(500) DEFAULT NULL,
   `categoria_id` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`producto_id`, `producto_codigo`, `producto_nombre`, `producto_stock_total`, `producto_tipo_unidad`, `producto_precio_compra`, `producto_precio_venta`, `producto_marca`, `producto_modelo`, `producto_estado`, `producto_foto`, `categoria_id`) VALUES
+(1, 'qwerty', 'Sabritas', 2, 'Unidad', 17.00, 20.00, 'Barcel', 'Sabritas Salsa', 'Habilitado', '', 1),
+(2, 'PROD_67d24f1be8dba', 'PLayera', -1, 'unidad', 0.00, 1200.00, 'Nike', 'Air Max', 'activo', NULL, 1),
+(3, 'PROD_67d27425612c3', 'short', -1, 'unidad', 0.00, 120.00, 'MIKE', 'Pumba', 'activo', NULL, 1),
+(4, 'PROD_67d27d2dc3c77', 'playera1', 0, 'unidad', 0.00, 120.00, '', '', 'activo', NULL, 1),
+(5, 'PROD_67da2ef913864', 'Playera', -1, 'unidad', 0.00, 1200.00, '', '', 'activo', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -122,12 +147,12 @@ CREATE TABLE `producto` (
 
 CREATE TABLE `usuario` (
   `usuario_id` int(7) NOT NULL,
-  `usuario_nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `usuario_apellido` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `usuario_email` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `usuario_usuario` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
-  `usuario_clave` varchar(535) COLLATE utf8_spanish2_ci NOT NULL,
-  `usuario_foto` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
+  `usuario_nombre` varchar(50) NOT NULL,
+  `usuario_apellido` varchar(50) NOT NULL,
+  `usuario_email` varchar(50) NOT NULL,
+  `usuario_usuario` varchar(30) NOT NULL,
+  `usuario_clave` varchar(535) NOT NULL,
+  `usuario_foto` varchar(200) NOT NULL,
   `caja_id` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
@@ -146,9 +171,9 @@ INSERT INTO `usuario` (`usuario_id`, `usuario_nombre`, `usuario_apellido`, `usua
 
 CREATE TABLE `venta` (
   `venta_id` int(30) NOT NULL,
-  `venta_codigo` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
+  `venta_codigo` varchar(200) NOT NULL,
   `venta_fecha` date NOT NULL,
-  `venta_hora` varchar(17) COLLATE utf8_spanish2_ci NOT NULL,
+  `venta_hora` varchar(17) NOT NULL,
   `venta_total` decimal(30,2) NOT NULL,
   `venta_pagado` decimal(30,2) NOT NULL,
   `venta_cambio` decimal(30,2) NOT NULL,
@@ -156,6 +181,14 @@ CREATE TABLE `venta` (
   `cliente_id` int(10) NOT NULL,
   `caja_id` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`venta_id`, `venta_codigo`, `venta_fecha`, `venta_hora`, `venta_total`, `venta_pagado`, `venta_cambio`, `usuario_id`, `cliente_id`, `caja_id`) VALUES
+(1, 'D7Y8L8F6Q5-1', '2025-03-13', '12:01 am', 1360.00, 1500.00, 140.00, 1, 1, 1),
+(2, 'M5N7P6V2M8-2', '2025-03-18', '08:42 pm', 1220.00, 1500.00, 280.00, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -169,10 +202,21 @@ CREATE TABLE `venta_detalle` (
   `venta_detalle_precio_compra` decimal(30,2) NOT NULL,
   `venta_detalle_precio_venta` decimal(30,2) NOT NULL,
   `venta_detalle_total` decimal(30,2) NOT NULL,
-  `venta_detalle_descripcion` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
-  `venta_codigo` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
+  `venta_detalle_descripcion` varchar(200) NOT NULL,
+  `venta_codigo` varchar(200) NOT NULL,
   `producto_id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `venta_detalle`
+--
+
+INSERT INTO `venta_detalle` (`venta_detalle_id`, `venta_detalle_cantidad`, `venta_detalle_precio_compra`, `venta_detalle_precio_venta`, `venta_detalle_total`, `venta_detalle_descripcion`, `venta_codigo`, `producto_id`) VALUES
+(1, 2, 17.00, 20.00, 40.00, 'Sabritas', 'D7Y8L8F6Q5-1', 1),
+(2, 1, 0.00, 1200.00, 1200.00, 'PLayera', 'D7Y8L8F6Q5-1', 2),
+(3, 1, 0.00, 120.00, 120.00, 'short', 'D7Y8L8F6Q5-1', 3),
+(4, 1, 0.00, 1200.00, 1200.00, 'Playera', 'M5N7P6V2M8-2', 5),
+(5, 1, 17.00, 20.00, 20.00, 'Sabritas', 'M5N7P6V2M8-2', 1);
 
 --
 -- Índices para tablas volcadas
@@ -248,7 +292,7 @@ ALTER TABLE `caja`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `categoria_id` int(7) NOT NULL AUTO_INCREMENT;
+  MODIFY `categoria_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `cliente`
@@ -260,13 +304,13 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `empresa_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `empresa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `producto_id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `producto_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -278,13 +322,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `venta_id` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `venta_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `venta_detalle`
 --
 ALTER TABLE `venta_detalle`
-  MODIFY `venta_detalle_id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `venta_detalle_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
